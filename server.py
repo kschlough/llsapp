3c0a9e2905b7dd01c8daa24e35ea00d17d06001b
kschlough <61514488+kschlough@users.noreply.github.com>
2021-01-28 22:47:00 -0800

    Added requirements.txt file, set up virtual env and installed Flask on local machine instead of using vagrant + got server.py running

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 """Server for LLS Man of the Year contest 2021 web app."""
 
 from flask import Flask, render_template, request, redirect
-import requests # python library - outgoing requests
+# import requests # python library - outgoing requests
 
 app = Flask(__name__)
 
 # team rosemead homepage
-@app.route('/'):
+@app.route('/')
 def homepage():
     """LLS Man of the Year homepage."""
 
     return render_template('rosemead_homepage.html')
+
+
+# need to import connect_to_db and db from model, if using
+if __name__ == '__main__':
+    # connect_to_db(app)
+    app.run()
