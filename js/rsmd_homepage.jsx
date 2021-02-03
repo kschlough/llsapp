19a9250661fd72e26ed6d10d1897dfbd9e9e931e
kschlough <61514488+kschlough@users.noreply.github.com>
2021-02-03 12:14:38 -0800

    pip3 installed Babel for jsx dependencies

--- js/rsmd_homepage.jsx
+++ js/rsmd_homepage.jsx
@@ -1,12 +1,16 @@
 function Homepage() {
     return (
-        <React.Fragment id="main_home">
-            <h1>LLS Man & Woman of the Year: Team Rosemead</h1>
-            <h2>Each year, all across the country, dedicated candidates engage in an exciting competition to earn The Leukemia & Lymphoma Society's (LLS) "Man and Woman of the Year" titles.</h2>
+        <React.Fragment>
+            <div className="wrapper">
+                <div className="container">
+                    <h1>LLS Man Woman of the Year: Team Rosemead</h1>
+                    <h2>Each year, all across the country, dedicated candidates engage in an exciting competition to earn The Leukemia Lymphoma Society's (LLS) "Man and Woman of the Year" titles.</h2>
 
-            <a href="https://www.mwoy.org/">Learn more on The Leukemia & Lymphoma Society's (LLS) website.</a>
+                    <a href="https://www.mwoy.org/">Learn more on The Leukemia Lymphoma Society's (LLS) website.</a>
 
-            <button>Donate</button>
+                    <button>Donate</button>
+                </div>
+            </div>
 
         </React.Fragment>
     );
