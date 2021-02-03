5d8790cc3c59be2ea05822b6aabb992017e1a988
kschlough <61514488+kschlough@users.noreply.github.com>
2021-02-03 15:48:24 -0800

    Added button onclick event to reroute any donate button to the donation page

---
+++ static/js/llsapp.js
@@ -0,0 +1,8 @@
+document.getElementById("donate").onclick = function() {Donate()};
+
+function Donate() {
+    document.getElementByID("donate").onclick="location.href='https://pages.lls.org/mwoy/gba/bayarea21/klof9w'"
+};
+
+
+
