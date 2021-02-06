84632ee2c62e24ba6df5e6c29604d3f30a86e1f9
kschlough <61514488+kschlough@users.noreply.github.com>
2021-02-06 10:14:03 -0800

    Changed LLS link to /about page

--- static/js/llsapp.js
+++ static/js/llsapp.js
@@ -1,8 +1,19 @@
-document.getElementById("donate").onclick = function() {Donate()};
+"use strict";
+
+// donate buttons
 
 function Donate() {
-    document.getElementByID("donate").onclick="location.href='https://pages.lls.org/mwoy/gba/bayarea21/klof9w'"
+    window.open('https://pages.lls.org/mwoy/gba/bayarea21/klof9w', '_blank');
 };
+
+$('#donate').on('click', Donate);
+
+d
+
+
+
+// '_blank' to open in same window
+
 
 
 
